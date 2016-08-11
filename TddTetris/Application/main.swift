483c715110f77b85af3eb8a98270325269757914
Robert Gravina <robert.gravina@gmail.com>
2016-08-11 14:04:19 +0900

    Create a testing app delegate.

---
+++ TddTetris/Application/main.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+if NSClassFromString("XCTestCase") != nil {
+    UIApplicationMain(
+        Process.argc,
+        Process.unsafeArgv,
+        NSStringFromClass(UIApplication),
+        NSStringFromClass(TestingAppDelegate)
+    )
+} else {
+    UIApplicationMain(
+        Process.argc,
+        Process.unsafeArgv,
+        NSStringFromClass(UIApplication),
+        NSStringFromClass(AppDelegate)
+    )
+}
