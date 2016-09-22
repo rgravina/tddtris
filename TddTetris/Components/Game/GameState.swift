ebdcceb1edeac15444f4ea7a220c780ae9a2dd13
Robert Gravina <robert.gravina@gmail.com>
2016-09-22 17:09:16 +0900

    add methods for managing state of cells to GameState

--- TddTetris/Components/Game/GameState.swift
+++ TddTetris/Components/Game/GameState.swift
@@ -13,4 +13,35 @@ class GameState {
             count: GameState.COLUMNS
         )
     }
+
+    func allocate(column: Int, row: Int) {
+        guard inBounds(column: column, row: row) else {
+            return
+        }
+        cells[column][row] = true
+    }
+
+    func free(column: Int, row: Int) {
+        guard inBounds(column: column, row: row) else {
+            return
+        }
+        cells[column][row] = false
+    }
+
+    func cell(column: Int, row: Int) -> Bool {
+        guard inBounds(column: column, row: row) else {
+            return false
+        }
+        return cells[column][row]
+    }
+
+    private func inBounds(column: Int, row: Int) -> Bool {
+        if (column >= 0 && column >= GameState.COLUMNS) {
+            return false
+        }
+        if (row >= 0 && row >= GameState.ROWS) {
+            return false
+        }
+        return true
+    }
 }
--- TddTetrisTests/Components/Game/GameStateTest.swift
+++ TddTetrisTests/Components/Game/GameStateTest.swift
@@ -14,4 +14,21 @@ class GameStateTest: XCTestCase {
         expect(self.state.cells.count).to(equal(GameState.COLUMNS))
         expect(self.state.cells[0].count).to(equal(GameState.ROWS))
     }
+
+    func test_setAndGetCellStatus() {
+        expect(self.state.cell(column: 0, row: 0)).to(equal(false))
+        state.allocate(column: 0, row: 0)
+        expect(self.state.cell(column: 0, row: 0)).to(equal(true))
+    }
+
+    func test_freeAnAllocatedCell() {
+        state.allocate(column: 0, row: 0)
+        state.free(column: 0, row: 0)
+        expect(self.state.cell(column: 0, row: 0)).to(equal(false))
+    }
+
+
+    func test_getCellOutOfBounds() {
+        expect(self.state.cell(column: GameState.COLUMNS+1, row: GameState.ROWS+1)).to(equal(false))
+    }
 }
